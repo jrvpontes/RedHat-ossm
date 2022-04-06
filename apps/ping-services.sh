3de9d7d421ec2134252168441caf46b6700ff2bd
João Pontes <jrvpontes@gmail.com>
2022-04-06 11:54:34 -0700

    apresentação-homol
    apresentação e implantação do service-mesh no ambiente de homologação.

--- apps/ping-services.sh
+++ apps/ping-services.sh
@@ -1,6 +1,6 @@
 #curl localhost:8080/bv
 #curl localhost:8888/order
-HOST=http://istio-ingressgateway-projeto-infra-ossm.apps.cluster-rfhmh.rfhmh.sandbox1884.opentlc.com
+HOST=http://istio-ingressgateway-projeto-infra-ossm.ocp.hml.proderj.rj.gov.br
 curl $HOST/order
 curl $HOST/spl50
 curl $HOST/rating
