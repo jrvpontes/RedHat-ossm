2c680cf6712ecf3b562179fd650f6f29d73c23f1
João Pontes <jrvpontes@gmail.com>
2022-04-06 15:00:34 -0700

    homol
    teste no ambiente de homol.

--- helm/ping-HOMOL.sh
+++ helm/ping-HOMOL.sh
@@ -1,6 +1,6 @@
 #curl localhost:8080/bv
 #curl localhost:8888/order
-HOST=http://istio-ingressgateway-projeto-infra-ossm.ocp.hml.proderj.rj.gov.br
+HOST=http://istio-ingressgateway-proderj-dit-infra-ossm.ocp.hml.proderj.rj.gov.br
 curl $HOST/order
 curl $HOST/spl50
 curl $HOST/rating
