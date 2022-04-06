b2744b20202898dabcf6b8a58ecad3281d3d54e9
João Pontes <jrvpontes@gmail.com>
2022-04-05 21:53:40 -0300

    beta-teste
    com 'projeto-infra-ossm' passou.

--- apps/ping-services.sh
+++ apps/ping-services.sh
@@ -1,6 +1,6 @@
 #curl localhost:8080/bv
 #curl localhost:8888/order
-HOST=http://istio-ingressgateway-detran-rj-infra-istio.apps.cluster-btnjd.btnjd.sandbox1087.opentlc.com
+HOST=http://istio-ingressgateway-projeto-infra-ossm.apps.cluster-rfhmh.rfhmh.sandbox1884.opentlc.com
 curl $HOST/order
 curl $HOST/spl50
 curl $HOST/rating
